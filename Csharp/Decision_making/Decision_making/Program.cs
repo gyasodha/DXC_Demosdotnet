51e5a707cd2608fae6ceb778a0b583bcbdec9768
Yasodha Priya <g.yasodhapriya@dxc.com>
2023-04-20 12:13:16 +0530

    updated

---
+++ Csharp/Decision_making/Decision_making/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Decision_making
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the three numbers");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2 && num1>num3)
+            {
+                Console.WriteLine("first number is greater");
+            }
+            else if (num2 > num3)
+            {
+                Console.WriteLine("Second number is greater");
+            }
+            else
+            {
+                Console.WriteLine("Third number is greater");
+            }
+        }
+    }
+}
---
+++ Csharp/vowel_constant/vowel_constant/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace vowel_constant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the alphabet");
+            char x = Convert.ToChar(Console.ReadLine());
+            if (x=='A' || x=='a' || x=='e' || x=='E' || x=='i' || x=='I' || x=='o' || x=='O' || x=='u' || x == 'U')
+            {
+                Console.WriteLine(x + " " + "is a vowel");
+            }
+            else
+            {
+                Console.WriteLine(x + " " + "is not a vowel");
+            }
+        }
+    }
+}
