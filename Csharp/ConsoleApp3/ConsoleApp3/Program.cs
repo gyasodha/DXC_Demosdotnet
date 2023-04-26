28eb324567cfd353ab1d0e74f8b872463880cf01
Yasodha Priya <g.yasodhapriya@dxc.com>
2023-04-26 16:52:51 +0530

    updated

---
+++ Csharp/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace OOP_Demo
+{
+    class Calculator
+    {
+        public void add()
+        {
+            Console.WriteLine("This is add method");
+        }
+
+        public void hlo(string name)
+        {
+            Console.WriteLine($"Hello {name}");
+        }
+
+        public void Sum(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public int Cube(int x)
+        {
+            return x * x * x;
+        }
+
+        public void Add(int a, int b = 0, int c = 0)
+        {
+            Console.WriteLine(a + b + c);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            calc.add();
+            calc.hlo("John");
+            calc.hlo("James");
+            calc.Sum(20, 20);
+            int res = calc.Cube(25);
+            Console.WriteLine(res);
+            calc.Add(10, 20, 40);
+        }
+    }
+}
---
+++ Csharp/oopsconcept/oopsconcept/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace oopsconcept
+{
+    class Operations
+    {
+        public void add (int a , int b)
+        {
+                Console.WriteLine (a+ b);
+        }
+        public int sub (int a=10,int b=20)
+        {
+            return a - b;
+        }
+        public int mul (int a,int b)
+        {
+            return a * b;
+        }
+        public void div(int a = 10, int b = 0)
+        {
+            Console.WriteLine(a / b);
+        }
+        public void md(int a , int b)
+        {
+            Console.WriteLine(a % b);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations op = new Operations();
+            op.add (1, 2);
+            int res = op.sub();
+            Console.WriteLine (res);
+            int res1 = op.mul (1, 2);
+            Console.WriteLine (res1);
+            op.div(10,10);
+            op.md(20, 5);
+        }
+    }
+}
